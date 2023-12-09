efd940698996cdd08cacfbb814ebb5ce708d9a7f
bhonkabigting <bhonkabigting@gmail.com>
2023-12-09 22:42:09 +0800

    Finished typography.

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,27 @@
+import { Typography } from "@mui/material";
+
+export const MuiTypography = () => {
+    return (
+        <div>
+            <Typography variant="h1">h1 Heading</Typography>
+            <Typography variant="h2">h2 Heading</Typography>
+            <Typography variant="h3">h3 Heading</Typography>
+            <Typography variant="h4" component="h1" gutterBottom>
+                h4 Heading
+            </Typography>
+            <Typography variant="h5">h5 Heading</Typography>
+            <Typography variant="h6">h6 Heading</Typography>
+
+            <Typography variant="subtitle1">Sub title 1</Typography>
+            <Typography variant="subtitle2">Sub title 2</Typography>
+
+            <Typography variant="body1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid cupiditate quam molestiae repellat delectus, aspernatur explicabo inventore, optio dolore id voluptatum reprehenderit, debitis ex similique assumenda unde incidunt? Enim, exercitationem! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo totam nobis dolorem nulla at consequatur deleniti accusantium soluta, odio exercitationem sit dolores consequuntur laborum. Labore quo rerum quidem saepe esse.
+            </Typography>
+            <Typography variant="body2">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, reiciendis! Illum ad quibusdam asperiores vero quasi sed ea sequi delectus corporis natus odit aliquam, ratione, rem necessitatibus, omnis consequatur quis? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, voluptatem, molestias facere velit fuga, corporis cupiditate quae deleniti perferendis omnis adipisci eveniet. Perspiciatis tenetur totam saepe laborum facilis exercitationem unde.
+            </Typography>
+        </div>
+    );
+};
+
