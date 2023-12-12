a5e0832dd2630ed0462193b61bc0c33c3440cf36
bhonkabigting <bhonkabigting@gmail.com>
2023-12-12 16:34:19 +0800

    Finished Tutorial-46-Customizing Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
+import { createTheme, colors, ThemeProvider } from "@mui/material";
 import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { MuiResponsiveness } from "./components/MuiResponsiveness";
 // import { MuiMasonry } from "./components/MuiMasonry";
 // import { MuiTimeline } from "./components/MuiTimeline";
@@ -39,49 +40,66 @@ import { MuiResponsiveness } from "./components/MuiResponsiveness";
 // import { MuiButton } from "./components/MuiButton";
 // import { MuiTypography } from "./components/MuiTypography";
 
+const theme = createTheme({
+	status: {
+		danger: "#e53e3e",
+	},
+	palette: {
+		secondary: {
+			main: colors.orange[500],
+		},
+		neutral: {
+			main: colors.grey[500],
+			darker: colors.grey[700],
+		},
+	},
+});
+
 function App() {
 	return (
-		<LocalizationProvider dateAdapter={AdapterDateFns}>
-			<div className="App">
-				{/* <MuiTypography /> */}
-				{/* <MuiButton /> */}
-				{/* <MuiTextField /> */}
-				{/* <MuiSelect /> */}
-				{/* <MuiRadioButton /> */}
-				{/* <MuiCheckbox /> */}
-				{/* <MuiSwitch /> */}
-				{/* <MuiRating /> */}
-				{/* <MuiAutocomplete /> */}
-				{/* <MuiLayout /> */}
-				{/* <MuiCard /> */}
-				{/* <MuiAccordion /> */}
-				{/* <MuiImageList /> */}
-				{/* <MuiNavbar /> */}
-				{/* <MuiBreadcrumbs /> */}
-				{/* <MuiDrawer /> */}
-				{/* <MuiLink /> */}
-				{/* <MuiSpeedDial /> */}
-				{/* <MuiBottomNavigation /> */}
-				{/* <MuiAvatar /> */}
-				{/* <MuiBadge /> */}
-				{/* <MuiList /> */}
-				{/* <MuiChip /> */}
-				{/* <MuiTooltip /> */}
-				{/* <MuiTable /> */}
-				{/* <MuiAlert /> */}
-				{/* <MuiSnackbar /> */}
-				{/* <MuiDialog /> */}
-				{/* <MuiProgress /> */}
-				{/* <MuiSkeleton /> */}
-				{/* <MuiLoadingButton /> */}
-				{/* <MuiPicker /> */}
-				{/* <MuiDateRangePicker /> */}
-				{/* <MuiTabs /> */}
-				{/* <MuiTimeline /> */}
-				{/* <MuiMasonry /> */}
-				<MuiResponsiveness />
-			</div>
-		</LocalizationProvider>
+		<ThemeProvider theme={theme}>
+			<LocalizationProvider dateAdapter={AdapterDateFns}>
+				<div className="App">
+					{/* <MuiTypography /> */}
+					{/* <MuiButton /> */}
+					{/* <MuiTextField /> */}
+					{/* <MuiSelect /> */}
+					{/* <MuiRadioButton /> */}
+					{/* <MuiCheckbox /> */}
+					{/* <MuiSwitch /> */}
+					{/* <MuiRating /> */}
+					{/* <MuiAutocomplete /> */}
+					{/* <MuiLayout /> */}
+					{/* <MuiCard /> */}
+					{/* <MuiAccordion /> */}
+					{/* <MuiImageList /> */}
+					{/* <MuiNavbar /> */}
+					{/* <MuiBreadcrumbs /> */}
+					{/* <MuiDrawer /> */}
+					{/* <MuiLink /> */}
+					{/* <MuiSpeedDial /> */}
+					{/* <MuiBottomNavigation /> */}
+					{/* <MuiAvatar /> */}
+					{/* <MuiBadge /> */}
+					{/* <MuiList /> */}
+					{/* <MuiChip /> */}
+					{/* <MuiTooltip /> */}
+					{/* <MuiTable /> */}
+					{/* <MuiAlert /> */}
+					{/* <MuiSnackbar /> */}
+					{/* <MuiDialog /> */}
+					{/* <MuiProgress /> */}
+					{/* <MuiSkeleton /> */}
+					{/* <MuiLoadingButton /> */}
+					{/* <MuiPicker /> */}
+					{/* <MuiDateRangePicker /> */}
+					{/* <MuiTabs /> */}
+					{/* <MuiTimeline /> */}
+					{/* <MuiMasonry /> */}
+					<MuiResponsiveness />
+				</div>
+			</LocalizationProvider>
+		</ThemeProvider>
 	);
 }
 
--- src/components/MuiTimeline.tsx
+++ src/components/MuiTimeline.tsx
@@ -5,30 +5,36 @@ import {
 	TimelineConnector,
 	TimelineContent,
 	TimelineDot,
-    TimelineOppositeContent
+	TimelineOppositeContent,
 } from "@mui/lab";
 
 export const MuiTimeline = () => {
 	return (
 		<Timeline position="alternate">
 			<TimelineItem>
-                    <TimelineOppositeContent color="text.secondary">09:30 am</TimelineOppositeContent>
+				<TimelineOppositeContent color="text.secondary">
+					09:30 am
+				</TimelineOppositeContent>
 				<TimelineSeparator>
 					<TimelineDot color="secondary" variant="outlined" />
 					<TimelineConnector />
 				</TimelineSeparator>
 				<TimelineContent>City A</TimelineContent>
 			</TimelineItem>
-            <TimelineItem>
-            <TimelineOppositeContent color="text.secondary">10:00 am</TimelineOppositeContent>
+			<TimelineItem>
+				<TimelineOppositeContent color="text.secondary">
+					10:00 am
+				</TimelineOppositeContent>
 				<TimelineSeparator>
 					<TimelineDot color="secondary" variant="outlined" />
 					<TimelineConnector />
 				</TimelineSeparator>
 				<TimelineContent>City B</TimelineContent>
 			</TimelineItem>
-            <TimelineItem>
-            <TimelineOppositeContent color="text.secondary">10:30 am</TimelineOppositeContent>
+			<TimelineItem>
+				<TimelineOppositeContent color="text.secondary">
+					10:30 am
+				</TimelineOppositeContent>
 				<TimelineSeparator>
 					<TimelineDot color="secondary" variant="outlined" />
 				</TimelineSeparator>
